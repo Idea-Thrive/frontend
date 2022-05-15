a9c8aa212d939d75d0ec2e280524cb603d035f91
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-05-16 00:08:22 +0430

    feat: initial implementation of login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from './theme';
+import { ChakraProvider, Container } from '@chakra-ui/react';
+import theme from '@chakra-ui/theme';
 import Router from './router/router';
 
 export const App = () => (
-  <Router>
-    <ChakraProvider theme={theme}>App</ChakraProvider>
-  </Router>
+  <ChakraProvider theme={theme}>
+    <Container h="100vh" maxW="container.xl" p={0} centerContent>
+      <Router></Router>
+    </Container>
+  </ChakraProvider>
 );
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,8 +3,25 @@ import { hasToken } from 'service/auth';
 import { useNavigate } from 'react-router-dom';
 import { setItem } from '../../utils/storage';
 import storageKeys from '../../service/storage';
+import IdeaLogo from '../../assets/idea.svg';
+import { ColorModeSwitcher } from 'service/color-mode-switcher';
+import {
+  Flex,
+  Image,
+  Heading,
+  VStack,
+  Text,
+  Input,
+  FormControl,
+  FormLabel,
+  Button,
+  Box,
+  useColorMode,
+  AspectRatio,
+} from '@chakra-ui/react';
 
 function Login() {
+  const { toggleColorMode } = useColorMode();
   const navigate = useNavigate();
   useEffect(() => {
     if (hasToken()) {
@@ -12,16 +29,53 @@ function Login() {
     }
   }, []);
 
-  const handleLoginClick = () => {
-    setItem(storageKeys.token, 'test');
+  const handleToggleThemeClick = () => {
+    toggleColorMode();
+  };
+
+  const handleSignInClick = () => {
+    console.log('sign in');
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <Flex
+      px={10}
+      py={{ sm: 0, md: 20 }}
+      h="full"
+      width="full"
+      justifyContent="space-around"
+      alignItems="center"
+    >
+      <Image
+        display={{ base: 'none', lg: 'block' }}
+        h="auto"
+        maxW={500}
+        mx={5}
+        src={IdeaLogo}
+        w="full"
+        userSelect="none"
+        pointerEvents="none"
+      />
 
-      <button onClick={handleLoginClick}>login</button>
-    </div>
+      <VStack w="full" spacing={8} flexBasis={{ base: '100%', lg: '50%' }}>
+        <Heading letterSpacing={3}>Idea Thrive</Heading>
+
+        <Text>Welcome to Idea Thrive</Text>
+
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input placeholder="example@gmail.com" />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input placeholder="Password" type="password" />
+        </FormControl>
+
+        <Button onClick={handleSignInClick}>Sign In</Button>
+        <Button onClick={handleToggleThemeClick}>Toggle theme</Button>
+      </VStack>
+    </Flex>
   );
 }
 

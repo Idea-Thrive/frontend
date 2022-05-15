5afcad0bdbdc68fa118158784ecfabed6228f330
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-05-15 20:17:50 +0430

    feat: implement remove token service

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,8 +1,12 @@
 import storageKeys from '../service/storage';
-import { hasItem } from '../utils/storage';
+import { hasItem, removeItem } from '../utils/storage';
 
 function hasToken(): boolean {
   return hasItem(storageKeys.token);
 }
 
-export { hasToken };
+function removeToken(key: string): void {
+  removeItem(key);
+}
+
+export { hasToken, removeToken };

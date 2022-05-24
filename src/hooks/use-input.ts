e8aa23dc02dd095f0f120feae3abe208a29627bb
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-05-25 00:56:38 +0430

    feat: improve use input hook

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -1,16 +1,53 @@
 import { ChangeEvent, useState } from 'react';
 
-function useInput(initialValue: string) {
-  const [value, setValue] = useState(initialValue);
+type useInputParams = {
+  initialValue: string;
+  validator: (value: string) => boolean;
+  errorMessage: string;
+  clearErrorOnChange?: boolean;
+};
+
+type useInputReturn = {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  error: string | null;
+  validate: () => boolean;
+};
+
+function useInput({
+  initialValue,
+  validator,
+  errorMessage,
+  clearErrorOnChange,
+}: useInputParams): useInputReturn {
+  const [value, setValue] = useState<string>(initialValue);
+  const [error, setError] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
+
     setValue(value);
+
+    if (clearErrorOnChange) {
+      setError(null);
+    }
+  };
+
+  const validate = () => {
+    const validationResult = validator(value);
+
+    if (validationResult === false) {
+      setError(errorMessage);
+    }
+
+    return validationResult;
   };
 
   return {
     value,
     onChange: handleChange,
+    error,
+    validate,
   };
 }
 

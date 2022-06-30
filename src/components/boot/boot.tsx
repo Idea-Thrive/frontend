d1729cba2a459d178c9c7a0fac600c7c57202aac
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-06-30 23:07:01 +0430

    feat: add splash and error boundary to boot

--- src/components/boot/boot.tsx
+++ src/components/boot/boot.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { ChakraProvider, Container } from '@chakra-ui/react';
+import { FC, useState } from 'react';
+import { ChakraProvider, Container, useToast } from '@chakra-ui/react';
 import theme from 'theme';
 import Router from 'router/router';
 import { getCurrentLanguage } from 'i18n/i18n';
@@ -13,21 +13,39 @@ import { ERROR_UNAUTHORIZED } from 'service/error-codes';
 import { STATUS_OK } from 'constants/constants';
 import { useDispatch } from 'react-redux';
 import { updateUser } from 'store/slices/app-slice';
+import SplashScreen from 'components/splash-screen';
+import t from 'i18n';
+import ErrorBoundary from 'components/error-boundary';
 
 const Boot: FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const toast = useToast();
   const dispatch = useDispatch();
+
   const { direction } = getCurrentLanguage();
 
   const getConfig = async () => {
+    setIsLoading(true);
     try {
       const { status, data } = await requestConfig();
-
       if (status === STATUS_OK) {
-        console.log(data);
         dispatch(updateUser(data));
+        setIsLoading(false);
       }
-    } catch (err) {
+    } catch (err: any) {
+      setError(err);
+      toast({
+        title: t('anErrorHasOccurred'),
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+        position: 'top',
+      });
       console.error(err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -35,10 +53,21 @@ const Boot: FC = () => {
     getConfig();
   });
 
-  return (
-    <ChakraProvider theme={theme}>
-      <Fonts />
+  const handleTryAgainClick = () => {
+    getConfig();
+  };
 
+  if (isLoading) {
+    return <SplashScreen />;
+  }
+
+  if (Boolean(error)) {
+    return <ErrorBoundary onTryAgainClick={handleTryAgainClick} />;
+  }
+
+  return (
+    <>
+      <Fonts />
       <Container
         className={direction === 'ltr' ? style.ltr : style.rtl}
         h="100vh"
@@ -46,11 +75,19 @@ const Boot: FC = () => {
         p={0}
         centerContent
       >
-        <Router></Router>
+        <Router />
       </Container>
-    </ChakraProvider>
+    </>
   );
 };
+
+function BootWithChakra() {
+  return (
+    <ChakraProvider theme={theme}>
+      <BootWithRedux />
+    </ChakraProvider>
+  );
+}
 
 function BootWithRedux() {
   return (
@@ -60,4 +97,4 @@ function BootWithRedux() {
   );
 }
 
-export default BootWithRedux;
+export default BootWithChakra;

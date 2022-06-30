57c58deeee63ba7e505a551770d95972edcdf425
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-06-30 22:29:22 +0430

    feat: add user to store

--- src/store/slices/app-slice/app-slice.ts
+++ src/store/slices/app-slice/app-slice.ts
@@ -1,23 +1,47 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-type App = {
+type User = {
+  company_id: string | null;
+  email: string | null;
+  first_name: string | null;
+  gender: string | null;
+  last_name: string | null;
+  password: string | null;
+  personnel_id: string | null;
+  phone_number: string | null;
+  photo_url: string | null;
   role: string | null;
 };
 
+type App = {
+  user: User;
+};
+
 const initialState: App = {
-  role: null,
+  user: {
+    company_id: null,
+    email: null,
+    first_name: null,
+    gender: null,
+    last_name: null,
+    password: null,
+    personnel_id: null,
+    phone_number: null,
+    photo_url: null,
+    role: null,
+  },
 };
 
 const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
-    updateUserRole(state, action: PayloadAction<string>) {
-      state.role = action.payload;
+    updateUser(state, action: PayloadAction<User>) {
+      state.user = action.payload;
     },
   },
 });
 
-export const { updateUserRole } = appSlice.actions;
+export const { updateUser } = appSlice.actions;
 
 export default appSlice.reducer;

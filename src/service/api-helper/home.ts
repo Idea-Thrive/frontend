f8ccfb849d1121468dfa26daaa38b6f824f86140
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-07-01 05:22:10 +0430

    feat: implemet get ideas api call

--- src/service/api-helper/home.ts
+++ src/service/api-helper/home.ts
@@ -1,5 +1,19 @@
 import request from '../request';
 
-export function getIdeas(): Promise<any> {
-  return request.get('api/ideas');
+type GetIdeasParams = {
+  companyId: number;
+  category: string;
+  size: number;
+  offset: number;
+};
+
+export function getIdeas({
+  companyId,
+  category,
+  size,
+  offset,
+}: GetIdeasParams): Promise<any> {
+  return request.get('ideas', {
+    params: { company_id: companyId, category, size, offset },
+  });
 }

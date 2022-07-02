fcf90de27f148d0a6e145f73f8096378bae157a9
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-07-03 03:41:01 +0430

    feat: implemet comment component

---
+++ src/components/comment/comment.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import { Comment as CommentType } from 'types/types';
+import {
+  Box,
+  HStack,
+  useColorModeValue,
+  VStack,
+  Text,
+  Flex,
+} from '@chakra-ui/react';
+import { FiStar } from 'react-icons/fi';
+
+interface CommentPros {
+  comment: CommentType;
+}
+
+const Comment: FC<CommentPros> = ({ comment }) => {
+  const textColor = useColorModeValue('gray.600', 'whiteAlpha.700');
+
+  return (
+    <Box w="full">
+      <VStack w="full">
+        <Flex w="full" justifyContent="space-between" alignItems="center">
+          <Text>{comment.description}</Text>
+          <HStack spacing={2}>
+            <Text fontSize="xl" display="inline">
+              {comment.score}
+            </Text>
+            <FiStar size={24} color="gold" />
+          </HStack>
+        </Flex>
+        <Box w="full" textAlign="start" textColor={textColor}>
+          <Text>{comment.created_at}</Text>
+        </Box>
+      </VStack>
+    </Box>
+  );
+};
+
+export default Comment;

aa12a7e6d45d651b97510712926beb815f5fd763
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-07-02 20:33:47 +0430

    feat: use accordian in company page

--- src/pages/settings/companies/companies.tsx
+++ src/pages/settings/companies/companies.tsx
@@ -1,13 +1,17 @@
 import { FC, useState } from 'react';
 import {
   Input,
-  Box,
   FormControl,
   FormLabel,
   FormErrorMessage,
   Heading,
   Button,
   useToast,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from '@chakra-ui/react';
 import useInput from 'hooks/use-input';
 import { isRequired, isNumber } from 'utils/validate';
@@ -106,65 +110,76 @@ const Companies: FC = () => {
   };
 
   return (
-    <Box w="full" textAlign="center">
-      <Heading size="md">{t('addNewCompany')}</Heading>
+    <Accordion w="full" textAlign="center">
+      <AccordionItem>
+        <AccordionButton p={4}>
+          <Heading size="md">{t('addNewCompany')}</Heading>
+          <AccordionIcon />
+        </AccordionButton>
 
-      <FormControl mb={4} isInvalid={Boolean(companyNameInput.error)}>
-        <FormLabel>{t('companyName')}</FormLabel>
-        <Input
-          type="text"
-          placeholder=""
-          value={companyNameInput.value}
-          onChange={companyNameInput.onChange}
-        />
-        {companyNameInput.error && (
-          <FormErrorMessage>{t(companyNameInput.error)}</FormErrorMessage>
-        )}
-      </FormControl>
+        <AccordionPanel shadow="md" pb={4}>
+          <FormControl mb={4} isInvalid={Boolean(companyNameInput.error)}>
+            <FormLabel>{t('companyName')}</FormLabel>
+            <Input
+              type="text"
+              placeholder=""
+              value={companyNameInput.value}
+              onChange={companyNameInput.onChange}
+            />
+            {companyNameInput.error && (
+              <FormErrorMessage>{t(companyNameInput.error)}</FormErrorMessage>
+            )}
+          </FormControl>
 
-      <FormControl mb={4} isInvalid={Boolean(ownerFirstNameInput.error)}>
-        <FormLabel>{t('ownerFirstName')}</FormLabel>
-        <Input
-          type="text"
-          placeholder=""
-          value={ownerFirstNameInput.value}
-          onChange={ownerFirstNameInput.onChange}
-        />
-        {ownerFirstNameInput.error && (
-          <FormErrorMessage>{t(ownerFirstNameInput.error)}</FormErrorMessage>
-        )}
-      </FormControl>
+          <FormControl mb={4} isInvalid={Boolean(ownerFirstNameInput.error)}>
+            <FormLabel>{t('ownerFirstName')}</FormLabel>
+            <Input
+              type="text"
+              placeholder=""
+              value={ownerFirstNameInput.value}
+              onChange={ownerFirstNameInput.onChange}
+            />
+            {ownerFirstNameInput.error && (
+              <FormErrorMessage>
+                {t(ownerFirstNameInput.error)}
+              </FormErrorMessage>
+            )}
+          </FormControl>
 
-      <FormControl mb={4} isInvalid={Boolean(ownerLastNameInput.error)}>
-        <FormLabel>{t('ownerLastName')}</FormLabel>
-        <Input
-          type="text"
-          placeholder=""
-          value={ownerLastNameInput.value}
-          onChange={ownerLastNameInput.onChange}
-        />
-        {ownerLastNameInput.error && (
-          <FormErrorMessage>{t(ownerLastNameInput.error)}</FormErrorMessage>
-        )}
-      </FormControl>
+          <FormControl mb={4} isInvalid={Boolean(ownerLastNameInput.error)}>
+            <FormLabel>{t('ownerLastName')}</FormLabel>
+            <Input
+              type="text"
+              placeholder=""
+              value={ownerLastNameInput.value}
+              onChange={ownerLastNameInput.onChange}
+            />
+            {ownerLastNameInput.error && (
+              <FormErrorMessage>{t(ownerLastNameInput.error)}</FormErrorMessage>
+            )}
+          </FormControl>
 
-      <FormControl mb={5} isInvalid={Boolean(ownerNationalIdInput.error)}>
-        <FormLabel>{t('ownerNationalId')}</FormLabel>
-        <Input
-          type="number"
-          placeholder=""
-          value={ownerNationalIdInput.value}
-          onChange={ownerNationalIdInput.onChange}
-        />
-        {ownerNationalIdInput.error && (
-          <FormErrorMessage>{t(ownerNationalIdInput.error)}</FormErrorMessage>
-        )}
-      </FormControl>
+          <FormControl mb={5} isInvalid={Boolean(ownerNationalIdInput.error)}>
+            <FormLabel>{t('ownerNationalId')}</FormLabel>
+            <Input
+              type="number"
+              placeholder=""
+              value={ownerNationalIdInput.value}
+              onChange={ownerNationalIdInput.onChange}
+            />
+            {ownerNationalIdInput.error && (
+              <FormErrorMessage>
+                {t(ownerNationalIdInput.error)}
+              </FormErrorMessage>
+            )}
+          </FormControl>
 
-      <Button isLoading={isLoading} onClick={handleSubmitCompanyClick}>
-        {t('submitCompany')}
-      </Button>
-    </Box>
+          <Button isLoading={isLoading} onClick={handleSubmitCompanyClick}>
+            {t('submitCompany')}
+          </Button>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
   );
 };
 

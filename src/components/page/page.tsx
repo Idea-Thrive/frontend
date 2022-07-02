1a82fe3e8ef72d842dbd696fab189dae305f74f2
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-07-02 19:31:32 +0430

    feat: make page padding customizable

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react';
 import { Box } from '@chakra-ui/react';
 
-const Page: FC = ({ children }) => {
+interface PageProps {
+  customHorizontalPadding?: number;
+}
+
+const Page: FC<PageProps> = ({ children, customHorizontalPadding = 5 }) => {
   return (
-    <Box px={{ base: 5, lg: 0 }} py={{ base: 3, md: 10 }} w="full">
+    <Box
+      px={{ base: customHorizontalPadding, lg: 0 }}
+      py={{ base: 3, md: 10 }}
+      w="full"
+    >
       {children}
     </Box>
   );

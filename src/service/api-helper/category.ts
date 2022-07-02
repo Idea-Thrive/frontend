5164a690e31080962e243a45a93aaa39092f1122
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-07-02 20:39:51 +0430

    feat: implement category api calls

--- src/service/api-helper/category.ts
+++ src/service/api-helper/category.ts
@@ -4,8 +4,35 @@ type GetAllCategoriesParams = {
   companyId: number;
 };
 
+type CreateCategory = {
+  name: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+};
+
+type DeleteCategory = {
+  id: string;
+};
+
 export function getAllCategories({
   companyId,
 }: GetAllCategoriesParams): Promise<any> {
   return request.get('categories', { params: { company_id: companyId } });
+}
+
+export function createCategory({
+  name,
+  description,
+}: CreateCategory): Promise<any> {
+  return request.post('categories', {
+    name,
+    description,
+    created_at: Date.now(),
+    updated_at: Date.now(),
+  });
+}
+
+export function deleteCategory({ id }: DeleteCategory): Promise<any> {
+  return request.delete(`categories/${id}`);
 }
